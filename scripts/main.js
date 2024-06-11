083b68aa68a52d87f61a7eedfcce1761a1a1303c
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-11 10:47:07 +0200

    build: comments added to clarify the code
    Exercise completed

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const imagePaths = ['images/01.webp' , 'images/02.webp', 'images/03.webp', 'images/04.webp', 'images/05.webp'];
 
-
+// Insert images in the page
 for (let i = 0 ; i < imagePaths.length ; i++) {
     const imageContainers = [document.getElementById('changing-image') , document.getElementsByClassName('thumbnail')[i]];
 
@@ -8,11 +8,13 @@ for (let i = 0 ; i < imagePaths.length ; i++) {
         const image = document.createElement('img');
         image.src = imagePaths[i];
         imageContainers[j].append(image);
-    
+        
+        // Add active class to the first images of the page
         i === 0 ? j === 0 ? image.classList.add("active") : imageContainers[j].classList.add("active") : null;
     }
 }
 
+// Function to check images with active class, remove the class and set as active the one behind or after
 function activeTheAdjacent(isOrderFromFirstToLast = true) {
     const PrimaryImages = document.querySelectorAll('#changing-image > img') , thumbnailContainers = document.getElementsByClassName('thumbnail');
 
@@ -36,6 +38,8 @@ function activeTheAdjacent(isOrderFromFirstToLast = true) {
 
 
 const nextBtn = document.getElementsByTagName('button');
+
+// Calling the function on click on arrow, for previous arrow passing false as argument to revert the logic of the function and obtaining the previous item instead of the next one
 nextBtn[0].addEventListener('click' , function () {
     activeTheAdjacent(false);
 });
