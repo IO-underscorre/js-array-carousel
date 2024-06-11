1cf121cf1e1ef6403d3b62e3388f2e31a94cd755
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-11 10:22:20 +0200

    feat: script to check what is the active image and to set as active the one behind or forward

--- scripts/main.js
+++ scripts/main.js
@@ -12,3 +12,31 @@ for (let i = 0 ; i < imagePaths.length ; i++) {
         i === 0 ? j === 0 ? image.classList.add("active") : imageContainers[j].classList.add("active") : null;
     }
 }
+
+function activeTheAdjacent(isOrderFromFirstToLast = true) {
+    const PrimaryImages = document.querySelectorAll('#changing-image > img') , thumbnailContainers = document.getElementsByClassName('thumbnail');
+
+    for(let i = 0 , found = false ; !found ; i++) {
+        if(PrimaryImages[i].className.includes('active')) {
+            found = true;
+            
+            PrimaryImages[i].classList.remove('active');
+            thumbnailContainers[i].classList.remove('active');
+            
+            if (isOrderFromFirstToLast) {
+                i + 1 < PrimaryImages.length ? PrimaryImages[i + 1].classList.add('active') : PrimaryImages[0].classList.add('active');
+                i + 1 < thumbnailContainers.length ? thumbnailContainers[i + 1].classList.add('active') : thumbnailContainers[0].classList.add('active');
+            } else {
+                i - 1 >= 0 ? PrimaryImages[i - 1].classList.add('active') : PrimaryImages[PrimaryImages.length - 1].classList.add('active');
+                i - 1 >= 0 ? thumbnailContainers[i - 1].classList.add('active') : thumbnailContainers[thumbnailContainers.length - 1].classList.add('active');
+            }
+        }
+    }
+}
+
+
+const nextBtn = document.getElementsByTagName('button');
+nextBtn[0].addEventListener('click' , function () {
+    activeTheAdjacent(false);
+});
+nextBtn[1].addEventListener('click' , activeTheAdjacent);
