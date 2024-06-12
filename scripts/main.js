4017a0844472feef6ec51a204769167aad473898
Jacopo Scorretti <jacoposcorretti.99@gmail.com>
2024-06-12 12:00:02 +0200

    fix(activeTheAdjacent): global variables passed as arguments
    Possible errors while running fixed

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
 const imagePaths = ['images/01.webp' , 'images/02.webp', 'images/03.webp', 'images/04.webp', 'images/05.webp'];
-const largeImageContainer = document.getElementById('changing-image') , thumbnailContainers = document.getElementsByClassName('thumbnail');
+const primaryImageContainer = document.getElementById('changing-image') , thumbnailContainers = document.getElementsByClassName('thumbnail');
 
 // Insert images in the page
 for (let i = 0 ; i < imagePaths.length ; i++) {
-    const imageContainers = [largeImageContainer , thumbnailContainers[i]];
+    const imageContainers = [primaryImageContainer , thumbnailContainers[i]];
 
     for (let j = 0 ; j < 2 ; j++) {
         const image = document.createElement('img');
@@ -18,22 +18,22 @@ for (let i = 0 ; i < imagePaths.length ; i++) {
 const primaryImages = document.querySelectorAll('#changing-image > img');
 
 // Function to check images with active class, remove the class and set as active the one behind or after
-function activeTheAdjacent(isOrderFromFirstToLast = true) {
+function activeTheAdjacent(largeImages , smallImageContainers , isOrderFromFirstToLast = true) {
     for(let i = 0 , found = false ; !found ; i++) {
-        if(primaryImages[i].className.includes('active')) {
+        if(largeImages[i].className.includes('active')) {
             found = true;
             
-            primaryImages[i].classList.remove('active');
-            thumbnailContainers[i].classList.remove('active');
+            largeImages[i].classList.remove('active');
+            smallImageContainers[i].classList.remove('active');
             
             if (isOrderFromFirstToLast) {
-                const index = i + 1 < primaryImages.length ? i + 1 : 0;
-                primaryImages[index].classList.add('active');
-                thumbnailContainers[index].classList.add('active');
+                const index = i + 1 < largeImages.length ? i + 1 : 0;
+                largeImages[index].classList.add('active');
+                smallImageContainers[index].classList.add('active');
             } else {
-                const index = i - 1 >= 0 ? i - 1 : primaryImages.length - 1;
-                primaryImages[index].classList.add('active');
-                thumbnailContainers[index].classList.add('active');
+                const index = i - 1 >= 0 ? i - 1 : largeImages.length - 1;
+                largeImages[index].classList.add('active');
+                smallImageContainers[index].classList.add('active');
             }
         }
     }
@@ -44,6 +44,8 @@ const btns = document.getElementsByTagName('button');
 
 // Calling the function on click on arrow, for previous arrow passing false as argument to revert the logic of the function and obtaining the previous item instead of the next one
 btns[0].addEventListener('click' , function () {
-    activeTheAdjacent(false);
+    activeTheAdjacent(primaryImages , thumbnailContainers , false);
 });
-btns[1].addEventListener('click' , activeTheAdjacent);
+btns[1].addEventListener('click' , function () {
+    activeTheAdjacent(primaryImages , thumbnailContainers);
+});
